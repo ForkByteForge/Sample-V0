5ef0683990acf5f95267a5eab8b6181e8e2e8872
Shreyansh Tripathy <shreyansh@zenskar.com>
2024-09-29 16:10:44 +0530

    deps: add initial project starter pack

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { useRoutes } from "react-router-dom";
+import { TailwindIndicator } from "./components/tailwind-indicator";
+
+const routes = [{ path: "/", element: <Home /> }];
+
+function Home() {
+  return (
+    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+      <h1 className="text-4xl font-bold">Welcome to Remix!</h1>
+      <p className="text-lg">
+        This is a starter template for a Remix app. You can start building your
+        app right away.
+      </p>
+    </section>
+  );
+}
+
+function App() {
+  const children = useRoutes(routes);
+
+  return (
+    <>
+      <div className="relative flex min-h-screen flex-col">
+        <div className="flex-1">{children}</div>
+      </div>
+      <TailwindIndicator />
+    </>
+  );
+}
+
+export default App;
