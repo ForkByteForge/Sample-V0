5c63c48235d517e8e67efae4e728dbad04618dab
Shreyansh Tripathy <116892456+shreyT19@users.noreply.github.com>
2024-10-02 04:15:03 +0530

    ✨(feat) Added resizable workflow layout for better usability (#5)
    * 🎨(chore) add figtree font
    
    * 📦️ (deps) added resizable layout library
    
    * 📦️ (deps) added Tailwind CSS Animated library
    
    * ✨(feat) Added resizable workflow layout for better usability
    
    * 📦 (chore) add tailwindwcss-animated to tailwindconfig

--- src/pages/Canvas/layouts/tabs/Canvas.tsx
+++ src/pages/Canvas/layouts/tabs/Canvas.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
-const Canvas = () => {
-  return <div>Canvas</div>;
+const Canvas: React.FC<
+  React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>
+> = (props) => {
+  return <div {...props}>Canvas</div>;
 };
 
 export default Canvas;
