ac44d4ffb2170105b8ae6589b47cd74e5157400f
Shreyansh Tripathy <116892456+shreyT19@users.noreply.github.com>
2024-10-02 17:47:09 +0530

    ✨(feat) add WSYIWYG editor to document part (#7)

--- src/pages/Canvas/layouts/tabs/Document.tsx
+++ src/pages/Canvas/layouts/tabs/Document.tsx
@@ -1,9 +1,32 @@
-import React from "react";
+import Editor from "@/components/ui/Editor/Editor";
+import useTheme from "@/hooks/useSelectTheme";
+import { cn } from "@/lib/utils";
+import { Block } from "@blocknote/core";
+import { useState } from "react";
+
+const initialContent = [
+  {
+    type: "heading",
+    content: "Untitled Document",
+    props: {
+      level: 2,
+    },
+  },
+  {
+    type: "paragraph",
+  },
+] as unknown as Block[];
 
 const Document: React.FC<
   React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>
 > = (props) => {
-  return <div {...props}>Document</div>;
+  const [data, setData] = useState<Block[]>(initialContent);
+
+  return (
+    <div {...props} className={cn("mt-2 px-3", props.className)}>
+      <Editor data={data} onChange={(_data) => setData(_data)} />
+    </div>
+  );
 };
 
 export default Document;
